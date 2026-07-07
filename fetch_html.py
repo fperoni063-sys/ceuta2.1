a6f10a0299e3206b5b1071d90855dffe317bfe09
Franco Peroni <franpeperoni196@gmail.com>
2026-07-07 20:16:44 -0300

    Actualizar contrasena de la web vieja en fetch_html.py

--- fetch_html.py
+++ fetch_html.py
@@ -2,7 +2,7 @@ import requests
 import io
 
 session = requests.Session()
-login_data = {'username': 'admceu', 'password': 'Ceu+125aH', 'button': 'Submit'}
+login_data = {'username': 'admceu', 'password': 'CEU.5jh89a', 'button': 'Submit'}
 r1 = session.post('https://www.ceuta.org.uy/admin/login/checklogin.php', data=login_data)
 r2 = session.get('https://www.ceuta.org.uy/admin/panel.php?pg=inscriptos_list')
 r2.encoding = r2.apparent_encoding
